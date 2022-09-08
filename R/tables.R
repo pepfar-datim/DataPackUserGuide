38ffb99ace1737a11b8e0ce4ea5edd3b6141d216
Christopher Nemarich <55459914+cnemarich@users.noreply.github.com>
2022-09-08 11:55:56 -0400

    Documentation

--- R/tables.R
+++ R/tables.R
@@ -1,3 +1,14 @@
+#' @export
+#' @title Create flextable from schema data
+#'
+#' @description Creates and properly formats a flextable table
+#' using input data from a \code{datapackr} Data Pack schema.
+#'
+#' @param t Table of schema data to be used.
+#' @param section Name to be placed atop the section.
+#'
+#' @return Formatted flextable table.
+#'
 make_table <- function(t, section = NULL) {
   t |>
     as.data.frame() |>
@@ -24,8 +35,20 @@ make_table <- function(t, section = NULL) {
                                 bookdown = TRUE)
 }
 
+#' @export
+#' @title Prepare schema data for table creation
+#'
+#' @description Takes in a sheet name and a list of columns and plucks
+#' this information from the Data Pack schema. Then pivots and prepares this
+#' data for use in a table in the User Guide.
+#'
+#' @param sheet_name The Data Pack sheet that is being referenced in the table.
+#' @param columns A list of columns to be included in the table.
+#'
+#' @return A table references the sheet and columns specified.
+#'
 prepare_table_data <- function(sheet_name = NULL, columns = NULL) {
-  
+
   schema <- userguide_schema()
 
   data <- schema %>%
@@ -42,6 +65,25 @@ prepare_table_data <- function(sheet_name = NULL, columns = NULL) {
   data
 }
 
+# TODO: this function below used to be dependent on whether an HTML site or
+# PDF document was being knit. In the HTML site, tables used to be one single
+# table with a scrollbar at the bottom. Maintaining this code for two different
+# types of tables became too much during COP22 but this would improve the user
+# experience to reimplement at some point.
+
+#' Break up long table into shorter sections
+#'
+#' @description Takes a long table with too many rows to fit across
+#' on one page and breaks it up into several shorter tables of
+#' appropriate length.
+#'
+#' @param t Table of schema data to be broken up.
+#' @param max_cols Maximum number of rows across allowed.
+#' @param is_html_knit Defines whether this is for a PDF
+#' output or an HTML output.
+#'
+#' @return A list of tables broken up into approriate sized chunks
+#'
 table_seq <- function(t = NULL,
                       max_cols = 6,
                       is_html_knit = knitr::is_html_output()) {
